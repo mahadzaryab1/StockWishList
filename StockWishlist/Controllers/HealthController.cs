27b8aa87638542a33834ff174c58a2c5f3e7349d
Mahad Zaryab <mahadzaryab1@gmail.com>
2020-12-29 18:32:38 -0500

    added health endpoint

---
+++ StockWishlist/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockWishlist.Models;
+
+namespace StockWishlist.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // GET: api/Health
+        [HttpGet]
+        public ActionResult<Status> Get()
+        {
+            return Ok(new Status() {ServerStatus = "Healthy!"});
+        }
+    }
+}
