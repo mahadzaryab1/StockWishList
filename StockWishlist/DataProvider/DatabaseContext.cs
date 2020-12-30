05741ae86c88ed47238484f7ce12587010b6dd83
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2020-12-29 22:33:40 -0500

    Added In-Memory Database and Boilerplate Controllers (#9)
    * added dependencies and configurations for in-memory database
    
    * refactored to get data from datbase in controllers via dependency injection

---
+++ StockWishlist/DataProvider/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
+using StockWishlist.DataProvider.Models;
+
+namespace StockWishlist.DataProvider
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : 
+            base(options) { }
+
+        public DbSet<StockWishlistEntry> StockWishlistEntries { get; set; }
+    }
+}
---
+++ StockWishlist/DataProvider/Models/StockWishlistEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockWishlist.DataProvider.Models
+{
+    public class StockWishlistEntry
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
