8f58c554c78856a8bcd43ff15f95d2cadb9575eb
Mahad Zaryab <mahadzaryab1@gmail.com>
2020-12-29 21:47:14 -0500

    added dependencies and configurations for in-memory database

---
+++ StockWishlist/DataProvider/Models/StockWishlistEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockWishlist.DataProvider.Models
+{
+    public class StockWishlistEntry
+    {
+        public Guid Id { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
